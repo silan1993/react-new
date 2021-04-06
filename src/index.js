8cc5711fd017aa98e35694c8b874ff75fa98fe21
silansubhra <subhrajyoti1903@gmail.com>
2021-04-07 00:23:30 +0530

    added hellow world

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import reactDom from "react-dom";
+
+const element = <h1>Hello World</h1>;
+
+console.log(element);
+reactDom.render(element, document.getElementById("root"));
